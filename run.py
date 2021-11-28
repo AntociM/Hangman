174efb5016b76af50bc867a68daaa7e6f74e98cb
AntociM <gidioimihaela@gmail.com>
2021-11-28 19:35:23 +0000

    Add the initial user data requests

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#!/home/gitpod/.pyenv/shims/python3
+def main():
+    word_to_guess = "gift"
+    rounds_nr = 0
+    score = 0
+    print("Hangman!")
+    name = input("Enter your name: ")
+    print(f"Welcome, {name}")
+    while True:
+        rounds_nr = input("Insert number of rounds from 1 to 10: ")
+        if rounds_nr.isnumeric():
+            break
+        else:
+            print("Oops! Try again")
+if __name__ == "__main__":
+    main()
