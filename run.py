54c7bcc50f782b13ce113efdccf952582b705023
AntociM <gidioimihaela@gmail.com>
2021-11-29 13:23:54 +0000

    Create get_random_word function to get a random word from a file

--- run.py
+++ run.py
@@ -1,32 +1,37 @@
-
+import random
 from screen import HANGMANPICS as hangman
 import os
 
-def game(name, rounds_nr, word_to_guess):
-    os.system("clear")
-    print(hangman[6])
+def get_random_word(file_name):
+    with open(file_name, "r") as file:
+        all_text = file.read()
+        words = list(map(str, all_text.split()))
+        word = random.choice(words)
+        return word
+
+
 
 def main():
-    word_to_guess = "gift"
     rounds_nr = 0
     score = 0
-    print("Hangman!")
-    name = input("Enter your name: ")
-    print(f"Welcome, {name}")
-    while True:
-        rounds_nr = input("Insert number of rounds from 1 to 10: ")
-        if rounds_nr.isnumeric():
-            break
-        else:
-            print("Oops! Try again")
+    print("Let's play Hangman!")
+    name = "Mihaela"
+    rounds_nr = 2
+    # Uncomment later
+    # name = input("Enter your name: ")
+    # print(f"Welcome, {name}")
+    # while True:
+        # rounds_nr = input("Insert number of rounds from 1 to 10: ")
+        # if rounds_nr.isnumeric():
+            # break
+        # else:
+            # print("Oops! Try again")
+    word123456 = get_random_word("words.txt")
+    
 
-    game(name, rounds_nr, word_to_guess)
-
-
-# def update_board(name, rounds_nr, word_to_guess, strikes, score):
 
 # class screen():
-#     print("Strikes:")
+    # print("Strikes:")
 
 if __name__ == "__main__":
     main()
